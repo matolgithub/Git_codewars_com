60b3430247cdcdb94a8a4b7b1abf5cc1d3fe6e0e
matolgithub <89175955650@mail.ru>
2022-09-08 16:18:59 +0300

    Final commit for who_like_it programe.

--- who_like_ it_080922.py
+++ who_like_ it_080922.py
@@ -1,5 +1,16 @@
 def likes(names=["Alex", "Jacob", "Mark", "Max"] ):
-    pass
+    if names == []:
+        return "no one likes this"
+    elif len(names) == 1:
+        return f"{names[0]} likes this"
+    elif len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif len(names) == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        count_users = len(names)
+        num_in_text = count_users - 2
+        return f"{names[0]}, {names[1]} and {num_in_text} others like this"    
 
 
 if __name__ == "__main__":
