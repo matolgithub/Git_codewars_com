1609c63ee86a1572c5b78cd8af2c3a9342d00560
matolgithub <89175955650@mail.ru>
2022-09-11 13:48:35 +0300

    First/last commit.

---
+++ break_camelCase_110922.py
@@ -0,0 +1,16 @@
+# def solution(s="ghghghHdjdjdLcncn"):
+#     new_string = ""
+#     for symbol in s:
+#         if symbol != symbol.upper():
+#             new_string += symbol
+#         else:
+#             new_string += symbol.replace(symbol, f" {symbol}")
+#     print(new_string)
+#     return new_string
+
+def solution(s="ghghghHdjdjdLcncn"):
+    print(''.join(' ' + c if c.isupper() else c for c in s))
+    return ''.join(' ' + c if c.isupper() else c for c in s)
+
+solution()
+
