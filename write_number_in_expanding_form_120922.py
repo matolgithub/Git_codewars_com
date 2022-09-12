7f6a21645687f367a90a79a484a54538a8fda3e6
matolgithub <89175955650@mail.ru>
2022-09-12 22:42:11 +0300

    Commit to git.

---
+++ write_number_in_expanding_form_120922.py
@@ -0,0 +1,23 @@
+def expanded_form(num=4982342):
+    result_string = ''
+    length = len(str(num))
+    num_list = [int(item) for item in list(str(num))]
+    count = 0
+    for number in num_list:
+        if number != 0:
+            if count == 0:
+                number  = number * (10 ** (length - 1))
+                result_string += str(number)
+            else:
+                number  = number * (10 ** (length - 1))
+                result_string += f" + {number}"
+            length -= 1 
+            count += 1
+        elif number == 0:
+            length -= 1 
+            count += 1
+    print(result_string)
+    return result_string
+
+
+expanded_form()
