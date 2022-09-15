14abcb8a97cf511a069e3377a7e9a05672861b37
matolgithub <89175955650@mail.ru>
2022-09-15 18:57:20 +0300

    Commit to git.

--- Find_The_Parity_Outlier_150922.py
+++ Find_The_Parity_Outlier_150922.py
@@ -3,8 +3,13 @@ def find_outlier(integers=[160, 3, 1719, 19, 11, 13, -21]):
         result = [item for item in integers if item % 2 != 0][0]
     elif len([item for item in integers if item % 2 != 1]) == 1:
         result = [item for item in integers if item % 2 != 1][0]
-    print(result)
+    # print(result)
     return result
 
+
+# def find_outlier(int):
+#     odds = [x for x in int if x%2!=0]
+#     evens= [x for x in int if x%2==0]
+#     return odds[0] if len(odds)<len(evens) else evens[0]
 
 find_outlier()
---
+++ common_denomination_150922.py
@@ -0,0 +1,34 @@
+def convert_fracts(lst={frozenset({1, 2}), frozenset({2, 3}), frozenset({1, 4})}):
+    if type(lst) == list:
+        new_lst = []
+    elif type(lst) == tuple:
+        new_lst = ()
+    elif type(lst) == dict:
+        new_lst = {}
+    list_denom = [item[1] for item in lst]
+    min_denom = 1
+    length_list = len(lst)
+
+    while True:
+        count = 0
+        for item in list_denom:
+            if min_denom % item == 0:
+                count += 1
+        if count == length_list:
+            break
+        else:
+            min_denom += 1
+        
+    for item in lst:
+        index = int(min_denom / item[1])
+        if type(item) == list:
+            new_lst.append([(item[0] * index), (item[1] * index)])
+        elif type(item) == tuple:
+            new_lst += (((item[0] * index), (item[1] * index)), )
+        elif type(item) == dict:
+            new_lst += frozenset({item[0] * index, item[1] * index})
+    print(new_lst)
+
+    return new_lst
+
+convert_fracts()
