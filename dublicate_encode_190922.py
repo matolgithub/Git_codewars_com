64bc1a814aa6158ecf626657e9e84db61b8cfabf
matolgithub <89175955650@mail.ru>
2022-09-19 17:58:35 +0300

    Commit to git.

---
+++ dublicate_encode_190922.py
@@ -0,0 +1,15 @@
+def duplicate_encode(word="Ffdgh(( @"):
+    new_str = ""
+    for symbol in word.lower():
+        if word.lower().count(symbol) == 1:
+            new_str += "("
+        elif word.lower().count(symbol) > 1:
+            new_str += ")"
+        # print(new_str)
+    # print(new_str)
+
+    return new_str
+
+
+
+duplicate_encode()
